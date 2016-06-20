813a3e1600f9bcbfd157f3afca968f098a5445ca
Rene <cheesus928@gmail.com>
2016-06-19 20:47:06 -0500

    Added 2nd function and changed the name of the 1st one

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,4 +1,4 @@
-def bubble_sort2(to_sort)
+def bubble_sort(to_sort)
     i = 0
     sorted = []
     while to_sort.empty? == false
@@ -9,6 +9,26 @@ def bubble_sort2(to_sort)
             left = to_sort[i]
             right = to_sort[i+1]
             if left > right
+                to_sort[i] = right
+                to_sort[i+1] = left
+            end
+            i += 1
+        end
+    end
+    print sorted
+end
+
+def bubble_sort_by(to_sort)
+    i = 0
+    sorted = []
+    while to_sort.empty? == false
+        if i == to_sort.length - 1
+            sorted.unshift(to_sort.pop())
+            i = 0
+        else
+            left = to_sort[i]
+            right = to_sort[i+1]
+            if yield(left,right) > 0
                 to_sort[i] = right
                 to_sort[i+1] = left
             end
